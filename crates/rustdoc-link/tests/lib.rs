26eca5736519b4f58e078a995a85349b3cedec89
Tony Wu <tonywu6@protonmail.com>
2025-03-18 06:38:23 +0800

    add whitespace check in test

--- crates/rustdoc-link/tests/lib.rs
+++ crates/rustdoc-link/tests/lib.rs
@@ -1,13 +1,41 @@
-use anyhow::{Ok, Result};
+use anyhow::{bail, Result};
 
 use mdbook_rustdoc_link::{env::Environment, logger::ConsoleLogger, Client, ClientConfig};
+use similar::{ChangeTag, TextDiff};
 use tap::Pipe;
 use tokio::task::JoinSet;
 use util_testing::{portable_snapshots, test_document, TestDocument};
 
 async fn snapshot(client: Client, TestDocument { source, name }: TestDocument) -> Result<()> {
     let output = client.process(source).await?;
-    portable_snapshots!().test(|| insta::assert_snapshot!(name, output))
+
+    portable_snapshots!().test(|| insta::assert_snapshot!(name, output))?;
+
+    invariant_whitespace(source, &output)?;
+
+    Ok(())
+}
+
+fn invariant_whitespace(source: &str, output: &str) -> Result<()> {
+    let changed_lines = TextDiff::from_words(source, output)
+        .iter_all_changes()
+        .filter_map(|change| {
+            if matches!(change.tag(), ChangeTag::Equal) {
+                return None;
+            }
+            if change.value().contains('\n') {
+                Some(change.value())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    if !changed_lines.is_empty() {
+        bail!("unexpected whitespace change: {changed_lines:?}")
+    } else {
+        Ok(())
+    }
 }
 
 #[tokio::test]
