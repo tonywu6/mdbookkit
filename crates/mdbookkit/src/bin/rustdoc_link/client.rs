c624b9efb428bd3ab5d19f5ca22379a968bf4c77
Tony Wu <tonywu6@protonmail.com>
2025-03-31 01:37:10 +0800

    [rustdoc-link] try to fix spurious RA progress reports

--- crates/mdbookkit/src/bin/rustdoc_link/client.rs
+++ crates/mdbookkit/src/bin/rustdoc_link/client.rs
@@ -122,25 +122,18 @@ impl Server {
         fn on_progress(state: &mut State, progress: ProgressParams) {
             match indexing_progress(&progress) {
                 Some(WorkDoneProgress::Begin(begin)) => {
-                    state.percent_indexed = dbg!(Some(0));
+                    state.percent_indexed = Some(0);
 
                     let msg = begin.message.as_deref().unwrap_or_default();
-                    spinner().update(ra_spinner!(), dbg!(msg));
+                    spinner().update(ra_spinner!(), msg);
 
                     let tx = state.tx.clone();
                     tokio::spawn(async move { tx.send(Poll::Pending).await.ok() });
                 }
 
                 Some(WorkDoneProgress::Report(report)) => {
-                    if let Some(msg) = dbg!(&report.message) {
-                        spinner().update(ra_spinner!(), dbg!(msg));
-
-                        // HACK: invalidate progress reports that say "0/1 (crate name)"
-                        // because RA isn't actually indexing everything at this point
-                        if msg.contains("0/1") {
-                            state.percent_indexed = None;
-                            return;
-                        }
+                    if let Some(msg) = &report.message {
+                        spinner().update(ra_spinner!(), msg);
                     }
 
                     let Some(indexed) = state.percent_indexed.as_mut() else {
@@ -148,15 +141,29 @@ impl Server {
                         return;
                     };
 
-                    if let Some(pc) = dbg!(report.percentage) {
-                        if pc >= *indexed {
-                            *indexed = pc;
-                        }
+                    let update = report.percentage.unwrap_or(0);
+
+                    let spurious = if let Some(msg) = &report.message {
+                        // HACK: invalidate progress reports that say "0/1 (crate name)"
+                        // because RA isn't actually indexing everything at this point
+                        msg.contains("0/1")
+                    } else {
+                        // also ignore indexing runs that went from 0 to a 100
+                        *indexed == 0 && update == 100
+                    };
+
+                    if spurious {
+                        state.percent_indexed = None;
+                        return;
+                    }
+
+                    if update >= *indexed {
+                        *indexed = update;
                     }
                 }
 
                 Some(WorkDoneProgress::End(end)) => {
-                    let Some(indexed) = dbg!(state.percent_indexed) else {
+                    let Some(indexed) = state.percent_indexed else {
                         // progress was invalidated
                         return;
                     };
@@ -166,7 +173,7 @@ impl Server {
                     }
 
                     let msg = end.message.as_deref().unwrap_or("indexing done");
-                    spinner().update(ra_spinner!(), dbg!(msg));
+                    spinner().update(ra_spinner!(), msg);
 
                     let tx = state.tx.clone();
                     tokio::spawn(async move { tx.send(Poll::Ready(())).await.ok() });
