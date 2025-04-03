a9905420e685b492bee89ec6d0b3dcd40f444da0
Tony Wu <tonywu6@protonmail.com>
2025-04-03 23:35:12 +0800

    chore: finish setting up pages?

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,9 +1,19 @@
+// @ts-check
+
 /** @type {import("prettier").Config} */
 const config = {
   printWidth: 88,
   proseWrap: "always",
   tabWidth: 2,
   useTabs: false,
+  overrides: [
+    {
+      files: [".github/workflows/**/*.yml"],
+      options: {
+        proseWrap: "preserve",
+      },
+    },
+  ],
 };
 
 export default config;
