4bfb84b596155b3d2b30ba28702ad7fe30bbcf9e
Tony Wu <tonywu6@protonmail.com>
2025-12-08 17:29:11 +0800

    docs: cleanup

--- docs/src/lib.rs
+++ docs/src/lib.rs
@@ -1,11 +1,1 @@
-//! This crate is only here to provide a context for mdbook-rustdoc-link to resolve items from.
 
-#![allow(unused)]
-
-use anyhow::Context;
-
-use mdbookkit::bin::rustdoc_link::Resolver;
-
-mod env {
-    pub use mdbookkit::{bin::rustdoc_link::env::Config, error::is_ci};
-}
