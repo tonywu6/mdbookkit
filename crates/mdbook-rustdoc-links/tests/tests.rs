8f5ad6be744bcaeab19bda43f3e3e23ea9a4cafd
Tony Wu <tonywu6@protonmail.com>
2026-04-12 09:57:53 +0800

    adjust svg output

--- crates/mdbook-rustdoc-links/tests/tests.rs
+++ crates/mdbook-rustdoc-links/tests/tests.rs
@@ -1,8 +1,9 @@
 use mdbookkit_testing::{snapbox::file, test_mdbook};
 
 test_mdbook![
-    out_of_the_box,
-    stderr.svg = file!["out_of_the_box/stderr/data.svg": TermSvg],
-    stderr.txt = file!["out_of_the_box/stderr/data.txt": Text],
-    rendered = [file!["out_of_the_box/out/chapter_1.md": Text]],
+    rustdoc,
+    exit(0),
+    stderr.svg = file!["rustdoc/stderr/data.svg": TermSvg],
+    stderr.txt = file!["rustdoc/stderr/data.txt": Text],
+    rendered = [file!["rustdoc/out/chapter_1.md": Text]],
 ];
--- crates/mdbookkit-testing/src/lib.rs
+++ crates/mdbookkit-testing/src/lib.rs
@@ -1,15 +1,20 @@
-use std::path::PathBuf;
+use std::{fmt::Display, path::PathBuf};
 
-use anstream::adapter::strip_bytes;
+use anstyle::RgbColor;
+use anstyle_svg::Palette;
 use anyhow::{Context, Result};
 use regex::Regex;
-use snapbox::{Assert, Data, Redactions, assert::DEFAULT_ACTION_ENV, cmd::Command, dir::DirRoot};
+use snapbox::{
+    Assert, Data, Redactions, assert::DEFAULT_ACTION_ENV, cmd::Command, data::DataFormat,
+    dir::DirRoot,
+};
 
 pub use anyhow;
 pub use snapbox;
 
 pub struct TestBook<'a> {
     pub name: &'a str,
+    pub code: i32,
     pub root_dir: PathBuf,
     pub env_vars: Vec<(&'a str, &'a str)>,
     pub stderr_txt: Data,
@@ -21,6 +26,7 @@ impl TestBook<'_> {
     pub fn run(self) -> Result<()> {
         let Self {
             name,
+            code,
             root_dir,
             env_vars,
             stderr_txt,
@@ -31,18 +37,6 @@ impl TestBook<'_> {
         let book_dir = root_dir.join(name);
         let temp_dir = DirRoot::mutable_temp()?;
         let dist_dir = book_dir.join("out");
-
-        let assert = {
-            let mut redactions = Redactions::new();
-            redactions.insert("[TEST_DIR]", &root_dir)?;
-            redactions.insert(
-                "[CARGO_DURATION]",
-                Regex::new(r"target\(s\) in (?<redacted>\d+\.\d+s)")?,
-            )?;
-            Assert::new()
-                .action_env(DEFAULT_ACTION_ENV)
-                .redact_with(redactions)
-        };
 
         Command::new(env!("CARGO"))
             .arg("clean")
@@ -55,27 +49,44 @@ impl TestBook<'_> {
             .arg(&book_dir)
             .current_dir(&root_dir)
             .env("MDBOOK_build__build_dir", temp_dir.path().unwrap())
-            .env("MDBOOK_LOG", "info,mdbookkit::diagnostics=debug")
+            .env("MDBOOK_LOG", "off,mdbookkit::diagnostics=info")
             .env("MDBOOKKIT_TERM_GRAPHICAL", "ascii")
             .env("FORCE_COLOR", "1")
+            .env("RUST_BACKTRACE", "0")
             .envs(env_vars)
             .assert()
-            .success();
+            .code(code);
 
-        let stderr_svg_data = &*result.get_output().stderr;
-        let stderr_txt_data = strip_bytes(stderr_svg_data).into_vec();
+        let assert = {
+            let mut redactions = Redactions::new();
+            redactions.insert("[TEST_DIR]", &root_dir)?;
+            redactions.insert("[CARGO_ELAPSED]", Regex::new(r"in (?<redacted>\d+\.\d+s)")?)?;
+            redactions.insert(
+                "[LLVM_COV_STDERR]",
+                Regex::new(r"(?<redacted>error: process didn't exit successfully:.*)")?,
+            )?;
+            Assert::new()
+                .action_env(DEFAULT_ACTION_ENV)
+                .redact_with(redactions)
+        };
+
+        let stderr = &*result.get_output().stderr;
+        let stderr = String::from_utf8_lossy(stderr);
 
         let mut results = vec![
-            assert.try_eq(None, stderr_txt_data.into(), stderr_txt),
-            assert.try_eq(None, stderr_svg_data.into(), stderr_svg),
+            assert.try_eq_text(None, &stderr, stderr_txt),
+            assert.try_eq_text(None, &stderr, stderr_svg),
         ];
 
         for expected in rendered {
-            let actual_path = expected.source().unwrap().as_path().unwrap();
-            let actual_path = actual_path.strip_prefix(&dist_dir)?.to_owned();
-            let actual_data = std::fs::read(temp_dir.path().unwrap().join(&actual_path))
-                .with_context(|| format!("no such page: {:?}", actual_path.display()))?;
-            results.push(assert.try_eq(Some(&actual_path.display()), actual_data.into(), expected));
+            let page = expected.source().unwrap().as_path().unwrap();
+            let page = page.strip_prefix(&dist_dir)?.to_owned();
+
+            let actual_path = temp_dir.path().unwrap().join(&page);
+            let actual = std::fs::read_to_string(actual_path)
+                .with_context(|| format!("no such page: {:?}", page.display()))?;
+
+            results.push(assert.try_eq_text(Some(&page.display()), actual, expected));
         }
 
         for result in results.iter() {
@@ -92,23 +103,93 @@ impl TestBook<'_> {
     }
 }
 
+trait AssertUtil {
+    fn try_eq_text<S: AsRef<str>>(
+        &self,
+        name: Option<&dyn Display>,
+        actual: S,
+        expected: Data,
+    ) -> snapbox::assert::Result<()>;
+}
+
+impl AssertUtil for Assert {
+    fn try_eq_text<S: AsRef<str>>(
+        &self,
+        name: Option<&dyn Display>,
+        actual: S,
+        expected: Data,
+    ) -> snapbox::assert::Result<()> {
+        if expected.format() == DataFormat::TermSvg {
+            let rendered = self.redactions().redact(actual.as_ref().trim_end());
+            let rendered = render_svg(&rendered);
+            let expected = expected.coerce_to(DataFormat::Text);
+            self.try_eq(name, rendered.into(), expected)
+        } else if expected.format() == DataFormat::Text {
+            let rendered = anstream::adapter::strip_str(actual.as_ref()).to_string();
+            self.try_eq(name, rendered.into(), expected)
+        } else {
+            self.try_eq(name, actual.as_ref().into(), expected)
+        }
+    }
+}
+
+fn render_svg(text: &str) -> String {
+    const fn rgb(r: u8, g: u8, b: u8) -> RgbColor {
+        RgbColor(r, g, b)
+    }
+
+    const BG_COLOR: RgbColor = rgb(26, 26, 26);
+    const FG_COLOR: RgbColor = rgb(178, 178, 178);
+
+    const PALETTE: Palette = Palette([
+        rgb(54, 60, 70),
+        rgb(224, 108, 117),
+        rgb(150, 196, 117),
+        rgb(209, 154, 102),
+        rgb(92, 173, 241),
+        rgb(198, 120, 221),
+        rgb(81, 181, 195),
+        rgb(211, 211, 211),
+        rgb(110, 112, 116),
+        rgb(224, 108, 117),
+        rgb(168, 220, 131),
+        rgb(244, 183, 127),
+        rgb(95, 183, 255),
+        rgb(224, 135, 251),
+        rgb(94, 211, 227),
+        rgb(250, 250, 250),
+    ]);
+
+    anstyle_svg::Term::new()
+        .bg_color(BG_COLOR.into())
+        .fg_color(FG_COLOR.into())
+        .palette(PALETTE)
+        .render_svg(text)
+        .replace(
+            "SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace;",
+            "Menlo, SF Mono, Liberation Mono, Consolas, ui-monospace, monospace;",
+        )
+}
+
 #[macro_export]
 macro_rules! test_mdbook {
     [
         $name:ident,
+        exit($code:literal),
         stderr.svg = $stderr_svg:expr,
         stderr.txt = $stderr_txt:expr,
-        rendered = [$( $data:expr ),*],
+        $( rendered = [$( $data:expr ),*], )?
         $( env = [$( $env_key:literal = $env_val:literal ),*], )?
     ] => {
         #[test]
         fn $name() -> $crate::anyhow::Result<()> {
             $crate::TestBook {
                 name: stringify!($name),
+                code: $code,
                 root_dir: $crate::snapbox::current_dir!(),
                 stderr_svg: $stderr_svg,
                 stderr_txt: $stderr_txt,
-                rendered: vec![$($data)*],
+                rendered: vec![$($($data)*)?],
                 env_vars: vec![$($(($env_key, $env_val))*)?],
             }
             .run()
