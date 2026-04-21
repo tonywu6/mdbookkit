be8aec94ab123f01ab861da781425ba15d45f72a
Tony Wu <tonywu6@protonmail.com>
2026-04-21 16:17:15 +0800

    make cargo stderr less noisy for some tests

--- crates/mdbook-rustdoc-links/tests/tests.rs
+++ crates/mdbook-rustdoc-links/tests/tests.rs
@@ -37,9 +37,24 @@ test_mdbook![
 ];
 
 test_mdbook![preludes_invalid, exit(101), redacted = [redacted()]];
-test_mdbook![compilation_error, exit(101), redacted = [redacted()]];
-test_mdbook![multi_stage_some_failed, exit(0), redacted = [redacted()]];
-test_mdbook![multi_stage_all_failed, exit(101), redacted = [redacted()]];
+test_mdbook![
+    compilation_error,
+    exit(101),
+    env = ["CARGO_TERM_QUIET" = "true"],
+    redacted = [redacted()]
+];
+test_mdbook![
+    multi_stage_some_failed,
+    exit(0),
+    env = ["CARGO_TERM_QUIET" = "true"],
+    redacted = [redacted()]
+];
+test_mdbook![
+    multi_stage_all_failed,
+    exit(101),
+    env = ["CARGO_TERM_QUIET" = "true"],
+    redacted = [redacted()]
+];
 
 #[test]
 fn rustdoc_parity() -> Result<()> {
